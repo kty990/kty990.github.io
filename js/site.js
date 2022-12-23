127e49f2127ebffd12b4fcb6cf13d1bffcb0df43
phxntxsmic <41134070+kty990@users.noreply.github.com>
2022-12-22 23:56:40 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,9 +1,18 @@
 let hamburger = document.getElementById("hamburger");
+const navContainer = document.getElementById("nav-container");
 
 if (!hamburger) {
     throw new Error("Unable to adjust for hamburger... must have been eaten!")
 }
 
+function toggle() {
+    if (navContainer.id == "nav-container") {
+        navContainer.id = "nav-container_active";
+    } else {
+        navContainer.id = "nav-container";
+    }
+}
+
 hamburger.addEventListener("mousedown", () => {
-    console.log("Mouse down");
+    toggle();
 });
