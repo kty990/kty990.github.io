fa0441bb9588f64ca70da60d930dcb5e3a8c7ecd
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-01-11 09:46:47 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,27 +1,11 @@
 let hamburger = document.getElementById("hamburger");
 const navContainer = document.getElementById("nav-container");
 
-let commandsNav = document.getElementById("cmds");
-let aboutNav = document.getElementById("about");
-let faqNav = document.getElementById("faq");
-
 function toggle() {
     if (navContainer.id == "nav-container") {
         navContainer.id = "nav-container_active";
     } else {
         navContainer.id = "nav-container";
-    }
-}
-
-async function setActive() {
-    await main();
-    let url = window.location.href;
-    if (url.indexOf("commands") != -1) {
-        commandsNav.id = "nav-active";
-    } else if (url.indexOf("about") != -1) {
-        aboutNav.id = "nav-active";
-    } else if (url.indexOf("faq") != -1) {
-        faqNav.id = "nav-active";
     }
 }
 
@@ -32,6 +16,21 @@ async function main() {
     let newelem = document.createElement("div");
     newelem.innerHTML = txt;
     oldelem.parentNode.replaceChild(newelem,oldelem);
+}
+
+async function setActive() {
+    await main();
+    let url = window.location.href;
+    let commandsNav = document.getElementById("cmds");
+    let aboutNav = document.getElementById("about");
+    let faqNav = document.getElementById("faq");
+    if (url.indexOf("commands") != -1) {
+        commandsNav.id = "nav-active";
+    } else if (url.indexOf("about") != -1) {
+        aboutNav.id = "nav-active";
+    } else if (url.indexOf("faq") != -1) {
+        faqNav.id = "nav-active";
+    }
 }
 
 window.addEventListener("resize", (event) => {
@@ -52,7 +51,7 @@ window.addEventListener('resize', () => {
   document.documentElement.style.setProperty('--vw', `${vw}px`);
 });
 
-setActive();
+setActive().catch(console.log);
 
 
 if (!hamburger) {
