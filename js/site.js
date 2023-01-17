f4fead16abf018074fdede3ed229a41ce7519810
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-01-17 09:59:45 -0700

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,7 +1,10 @@
-let hamburger = document.getElementById("hamburger");
-const navContainer = document.getElementById("nav-container");
+/* Debugging version control */
+let v = 4;
+console.log(`Version: ${v}`);
 
+/* Functions */
 function toggle() {
+    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
     if (navContainer.id == "nav-container") {
         navContainer.id = "nav-container_active";
     } else {
@@ -10,7 +13,7 @@ function toggle() {
 }
 
 async function main() {
-    let res = await fetch("/../navbar.html");
+    let res = await fetch("/../main/navbar.html");
     let txt = await res.text();
     let oldelem = document.getElementById("replace-with-navbar");
     let newelem = document.createElement("div");
@@ -24,16 +27,32 @@ async function setActive() {
     let commandsNav = document.getElementById("cmds");
     let aboutNav = document.getElementById("about");
     let faqNav = document.getElementById("faq");
+    let homeNav = document.getElementById("home");
+    let hamburger = document.getElementById("hamburger");
     if (url.indexOf("commands") != -1) {
         commandsNav.id = "nav-active";
     } else if (url.indexOf("about") != -1) {
         aboutNav.id = "nav-active";
     } else if (url.indexOf("faq") != -1) {
         faqNav.id = "nav-active";
+    } else if (url.indexOf("home") != -1) {
+        homeNav.id = "nav-active";
     }
+
+    if (!hamburger) {
+        throw new Error("The hamburger was eaten!");
+    }
+    
+    hamburger.addEventListener("mousedown", () => {
+        console.log("Clicked!");
+        toggle();
+    });
 }
 
+/* Events & Run */
+
 window.addEventListener("resize", (event) => {
+    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
     if (window.innerWidth <= 1080 && navContainer.id == "nav-container_active") {
         navContainer.id = "nav-container";
     }
@@ -52,12 +71,3 @@ window.addEventListener('resize', () => {
 });
 
 setActive().catch(console.log);
-
-
-if (!hamburger) {
-    throw new Error("Unable to adjust for hamburger... must have been eaten!")
-}
-
-hamburger.addEventListener("mousedown", () => {
-    toggle();
-});
