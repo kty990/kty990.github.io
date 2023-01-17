e0416d013ee715d8f9cbd77ebb4d21de475ab235
kty990 <masterhotdog9@gmail.com>
2023-01-17 15:12:26 -0700

    Commit #1 from git

---
+++ js/faq.js
@@ -0,0 +1,45 @@
+let dropdown_flex = document.getElementsByClassName("dropdown-flex")[0];
+let q_a = {
+    "This is the question":"This is the answer"
+}
+
+let faqs = [];
+
+let template = document.getElementsByTagName("template")[0];
+for (const [question, answer] of Object.entries(q_a)) {
+    let t = template.content.cloneNode(true);
+    let title = t.querySelector("div.title").querySelector("p");
+    let desc = t.querySelector("div.description").querySelector("p");
+    title.textContent = question;
+    desc.textContent = answer;
+    let newDiv = document.createElement("div");
+    newDiv.classList.add("dropdown");
+    newDiv.appendChild(title);
+    newDiv.appendChild(desc);
+    dropdown_flex.appendChild(newDiv);
+    faqs.push(newDiv);
+}
+
+let active_dropdowns = 0;
+
+function toggleFAQ(element) {
+    let currentDisplay = element.querySelector(".description").style.display;
+    if (currentDisplay == "block") {
+        element.querySelector(".description").style.display = 'none';
+        active_dropdowns--;
+    } else {
+        element.querySelector(".description").style.display = 'block';
+        active_dropdowns++;
+    }
+    document.querySelector(".dropdown-flex").style.padding = `0vh 0vh ${active_dropdowns * 5}vh 0vh`;
+}
+
+function activate(element) {
+    element.querySelector(".title").addEventListener("mousedown", () => {
+        toggleFAQ(element);
+    });
+}
+
+for (let x = 0; x < faqs.length; x++) {
+    activate(faqs[x]);
+}
---
+++ js/site.js
@@ -0,0 +1,112 @@
+/* Debugging version control */
+let v = 5;
+console.log(`Version: ${v}`);
+
+/* Functions */
+function toggle() {
+    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
+    let discordNav = document.getElementsByClassName("discord")[0] || document.getElementsByClassName("discord_nav_active")[0];
+
+    if (navContainer.id == "nav-container") {
+        navContainer.id = "nav-container_active";
+    } else {
+        navContainer.id = "nav-container";
+        if (discordNav.classList.contains("discord_nav_active")) {
+            toggleDiscord();
+            console.log("Toggled");
+        } else {
+            console.log(`List: ${discordNav.classList}\nContains: ${discordNav.classList.contains("discord-active")}\n`);
+        }
+    }
+}
+
+function toggleDiscord() {
+    let dropdown = document.getElementsByClassName("side-drop")[0] || document.getElementsByClassName("discord-active")[0];
+    let discordNav = document.getElementsByClassName("discord")[0] || document.getElementsByClassName("discord_nav_active")[0];
+
+    if (dropdown.classList.contains("discord-active")) {
+        dropdown.classList.remove("discord-active");
+        dropdown.classList.add("side-drop");
+        discordNav.classList.remove("discord_nav_active");
+        discordNav.classList.add("discord");
+    } else {
+        dropdown.classList.add('discord-active');
+        dropdown.classList.remove("side-drop");
+        discordNav.classList.add("discord_nav_active");
+        discordNav.classList.remove("discord");
+    }
+}
+
+async function main() {
+    let res = await fetch("/../main/navbar.html");
+    let txt = await res.text();
+    let oldelem = document.getElementById("replace-with-navbar");
+    if (!oldelem) {
+        return;
+    }
+    let newelem = document.createElement("div");
+    newelem.innerHTML = txt;
+    oldelem.parentNode.replaceChild(newelem,oldelem);
+}
+
+async function setActive() {
+    await main().catch(console.log);
+    
+    let url = window.location.href;
+    let commandsNav = document.getElementById("cmds");
+    let aboutNav = document.getElementById("about");
+    let faqNav = document.getElementById("faq");
+    let homeNav = document.getElementById("home");
+    let discordNav = document.getElementsByClassName("discord")[0] || document.getElementsByClassName("discord_nav_active")[0];
+    let hamburger = document.getElementById("hamburger");
+    if (url.indexOf("commands") != -1) {
+        commandsNav.id = "nav-active";
+        discordNav.id = "discord-page";
+    } else if (url.indexOf("about") != -1) {
+        aboutNav.id = "nav-active";
+        discordNav.id = "discord-page";
+    } else if (url.indexOf("faq") != -1) {
+        faqNav.id = "nav-active";
+        discordNav.id = "discord-page";
+    } else if (url.indexOf("home") != -1) {
+        homeNav.id = "nav-active";
+        discordNav.id = "discord";
+    }
+
+    if (!hamburger) {
+        throw new Error("The hamburger was eaten!");
+    }
+    
+    hamburger.addEventListener("mousedown", () => {
+        toggle();
+    });
+    
+    let discord = document.getElementById("discord") || document.getElementById("discord-page");
+
+    discord.addEventListener("mousedown", () => {
+        toggleDiscord();
+    });
+}
+
+/* Events & Run */
+
+window.addEventListener("resize", (event) => {
+    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
+    if (window.innerWidth <= 1080 && navContainer.id == "nav-container_active") {
+        navContainer.id = "nav-container";
+    }
+});
+
+let vh = window.innerHeight * 0.01;
+let vw = window.innerWidth * 0.01;
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty('--vw', `${vw}px`);
+
+window.addEventListener('resize', () => {
+  let vh = window.innerHeight * 0.01;
+  let vw = window.innerWidth * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty('--vw', `${vw}px`);
+});
+
+setActive().catch(console.log);
