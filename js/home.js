142493c1089bb317e0777e330e8ced0d34f63b23
kty990 <masterhotdog9@gmail.com>
2023-01-18 11:34:42 -0700

    Adding project list functionality

---
+++ js/home.js
@@ -0,0 +1,81 @@
+class Project {
+    constructor(name, properties) {
+        this.name = name;
+        this.properties = properties;
+    }
+
+    GetName() {
+        return this.name;
+    }
+}
+
+const colorField = {
+    "JS":"#f5e042",
+    "HTML":"#f56342",
+    "CSS":"#c040f7"
+}
+
+const projects = [
+    new Project("Homies Bot",{"JS":100}),
+    new Project("kty990.github.io",{"HTML":26.9,"JS":25.1,"CSS":48})
+]
+
+function GetColorFromLang(lang) {
+    return colorField[lang] || "#fff"
+}
+
+let target = document.getElementById("project-flex");
+for (let project of projects) {
+    let obj = document.createElement("div");
+    obj.classList.add("project");
+    let p = document.createElement("p");
+    p.style.color = "#fff";
+    p.style.textAlign = "center";
+    p.style.width = "100%";
+    p.style.height = "50%";
+    p.style.fontSize = "calc(var(--vh, 1vh) * 2)"
+    p.textContent = project.GetName();
+    let slide = document.createElement("div");
+    slide.style.position = "relative";
+    slide.style.width = "75%";
+    slide.style.height = "25%";
+    slide.style.left = "12.5%";
+    slide.style.display = "flex";
+    slide.style.flexDirection = "row";
+    // slide.style.marginTop = "3%";
+    slide.style.marginBottom = "5%";
+    for (const [key, value] of Object.entries(project.properties)) {
+        let lang = document.createElement("div");
+        lang.style.backgroundColor = GetColorFromLang(key);
+        lang.style.width = `${value}%`;
+        lang.style.height = "2px";
+        lang.style.padding = "0";
+        let empty = document.createElement("p");
+        empty.textContent = "If you are reading this... why?";
+        empty.style.display = "none";
+        lang.appendChild(empty);
+
+        slide.appendChild(lang);
+    }
+
+    obj.appendChild(p);
+    obj.appendChild(slide);
+    target.appendChild(obj);
+}
+
+let legendTarget = document.getElementById("project-legend");
+for (const [lang, color] of Object.entries(colorField)) {
+    let div = document.createElement('div');
+    div.style.width = "calc(var(--vh, 1vh) * 3)";
+    div.style.height = "calc(var(--vh, 1vh) * 3)";
+    div.style.textAlign = "center";
+    div.style.display = "flex";
+    div.style.justifyContent = "center";
+    div.style.alignItems = "center";
+    div.style.fontSize = "calc(var(--vh, 1vh) * 1)";
+    div.style.padding = "calc(var(--vh, 1vh) * 0.5)"
+    div.style.backgroundColor = color;
+    div.style.marginRight = "5px";
+    div.textContent = lang;
+    legendTarget.appendChild(div);
+}
