a5ee56dac7f4414ca2a63c2ef8e23862f1ce92da
kty990 <masterhotdog9@gmail.com>
2023-02-16 13:15:29 -0700

    Testing new method for project list implementation

--- js/home.js
+++ js/home.js
@@ -34,109 +34,125 @@ function getJSON(url, callback) {
 let myProjects = [];
 
 let repo_url = "https://api.github.com/users/kty990/repos"
-getJSON(repo_url, function(err,data) {
-    if (err != null) {
-        console.error(`Error in getJson occured: ${err}`);
-    } else {
-        for (let x = 0; x < data.length; x++) {
-            let repo_name = data[x].name;
-            let language_url = `https://api.github.com/repos/kty990/${repo_name}/languages`;
-            getJSON(language_url, function(e,d) {
-                if (e != null) {
-                    console.error(`Error in language obtain: ${e}`);
-                } else {
-                    let language_tmp = {};
-                    let total = 0;
-                    for (const [language, count] of Object.entries(d)) {
-                        console.log(`L: ${language}\tC: ${count}`);
-                        language_tmp[language] = count;
-                        total = total + count;
-                    }
-                    for (const [l,c] of Object.entries(language_tmp)) {
-                        language_tmp[l] = Math.floor(c/total);
-                    }
-                    myProjects.push(new Project(repo_name,language_tmp));
+
+function load_projects() {
+    return new Promise((resolve, reject) => {
+        getJSON(repo_url, function(err,data) {
+            if (err != null) {
+                console.error(`Error in getJson occured: ${err}`);
+                reject(`An error occured trying to load github url: ${err}`);
+            } else {
+                for (let x = 0; x < data.length; x++) {
+                    let repo_name = data[x].name;
+                    let language_url = `https://api.github.com/repos/kty990/${repo_name}/languages`;
+                    getJSON(language_url, function(e,d) {
+                        if (e != null) {
+                            console.error(`Error in language obtain: ${e}`);
+                        } else {
+                            let language_tmp = {};
+                            let total = 0;
+                            for (const [language, count] of Object.entries(d)) {
+                                console.log(`L: ${language}\tC: ${count}`);
+                                language_tmp[language] = count;
+                                total = total + count;
+                            }
+                            for (const [l,c] of Object.entries(language_tmp)) {
+                                language_tmp[l] = Math.floor(c/total);
+                            }
+                            myProjects.push(new Project(repo_name,language_tmp));
+                            if (myProjects.length == data.length) {
+                                resolve();
+                            }
+                        }
+                    });
                 }
-            });
+
+            }
+        });
+    });
+}
+
+async function main() {
+    await load_projects();
+
+    let target = document.getElementById("project-flex");
+    for (let project of myProjects) {
+        let obj = document.createElement("div");
+        obj.classList.add("project");
+        let p = document.createElement("p");
+        p.style.color = "#fff";
+        p.style.textAlign = "center";
+        p.style.width = "100%";
+        p.style.height = "50%";
+        p.style.fontSize = "calc(var(--vh, 1vh) * 2)"
+        p.textContent = project.GetName();
+        let slide = document.createElement("div");
+        slide.style.position = "relative";
+        slide.style.width = "75%";
+        slide.style.height = "25%";
+        slide.style.left = "12.5%";
+        slide.style.display = "flex";
+        slide.style.flexDirection = "row";
+        slide.style.marginTop = "calc(var(--vh,1vh)*-2)";
+        slide.style.marginBottom = "10%";
+        let x = 0;
+        for (const [key, value] of Object.entries(project.properties)) {
+            let lang = document.createElement("div");
+            if (x == 0) {
+                lang.style.borderBottomLeftRadius = "50px";
+                lang.style.borderTopLeftRadius = "50px";
+            } 
+            if (x == Object.keys(project.properties).length - 1 || Object.keys(project.properties).length == 1) {
+                lang.style.borderBottomRightRadius = "50px";
+                lang.style.borderTopRightRadius = "50px";
+            }
+            console.log(`Check:\tX: ${x}\project.properties.keys.length: ${Object.keys(project.properties).length}`);
+            x++;
+            lang.style.backgroundColor = GetColorFromLang(key);
+            lang.style.width = `${value}%`;
+            lang.style.height = "calc(var(--vh,1vh)*2)";
+            lang.style.padding = "0";
+            lang.textContent = `${value}%`;
+            lang.style.fontSize = "calc(var(--vwh,1vh)*1.5)";
+            lang.style.fontFamily = "'Yanone Kaffeesatz', sans-serif";
+            lang.style.textAlign = "center";
+            lang.style.lineHeight = "calc(var(--vh,1vh)*2.5)"
+            let empty = document.createElement("p");
+            empty.textContent = "If you are reading this... why?";
+            empty.style.display = "none";
+            lang.appendChild(empty);
+
+            slide.appendChild(lang);
         }
+
+        obj.appendChild(p);
+        obj.appendChild(slide);
+        target.appendChild(obj);
     }
-});
+}
+
+main();
 
 function GetColorFromLang(lang) {
     return colorField[lang] || "#fff"
 }
 
-let target = document.getElementById("project-flex");
-for (let project of myProjects) {
-    let obj = document.createElement("div");
-    obj.classList.add("project");
-    let p = document.createElement("p");
-    p.style.color = "#fff";
-    p.style.textAlign = "center";
-    p.style.width = "100%";
-    p.style.height = "50%";
-    p.style.fontSize = "calc(var(--vh, 1vh) * 2)"
-    p.textContent = project.GetName();
-    let slide = document.createElement("div");
-    slide.style.position = "relative";
-    slide.style.width = "75%";
-    slide.style.height = "25%";
-    slide.style.left = "12.5%";
-    slide.style.display = "flex";
-    slide.style.flexDirection = "row";
-    slide.style.marginTop = "calc(var(--vh,1vh)*-2)";
-    slide.style.marginBottom = "10%";
-    let x = 0;
-    for (const [key, value] of Object.entries(project.properties)) {
-        let lang = document.createElement("div");
-        if (x == 0) {
-            lang.style.borderBottomLeftRadius = "50px";
-            lang.style.borderTopLeftRadius = "50px";
-        } 
-        if (x == Object.keys(project.properties).length - 1 || Object.keys(project.properties).length == 1) {
-            lang.style.borderBottomRightRadius = "50px";
-            lang.style.borderTopRightRadius = "50px";
-        }
-        console.log(`Check:\tX: ${x}\project.properties.keys.length: ${Object.keys(project.properties).length}`);
-        x++;
-        lang.style.backgroundColor = GetColorFromLang(key);
-        lang.style.width = `${value}%`;
-        lang.style.height = "calc(var(--vh,1vh)*2)";
-        lang.style.padding = "0";
-        lang.textContent = `${value}%`;
-        lang.style.fontSize = "calc(var(--vwh,1vh)*1.5)";
-        lang.style.fontFamily = "'Yanone Kaffeesatz', sans-serif";
-        lang.style.textAlign = "center";
-        lang.style.lineHeight = "calc(var(--vh,1vh)*2.5)"
-        let empty = document.createElement("p");
-        empty.textContent = "If you are reading this... why?";
-        empty.style.display = "none";
-        lang.appendChild(empty);
-
-        slide.appendChild(lang);
-    }
-
-    obj.appendChild(p);
-    obj.appendChild(slide);
-    target.appendChild(obj);
-}
-
-let legendTarget = document.getElementById("project-legend");
-for (const [lang, color] of Object.entries(colorField)) {
-    let div = document.createElement('div');
-    div.style.width = "calc(var(--vh, 1vh) * 3)";
-    div.style.height = "calc(var(--vh, 1vh) * 3)";
-    div.style.textAlign = "center";
-    div.style.display = "flex";
-    div.style.justifyContent = "center";
-    div.style.alignItems = "center";
-    div.style.fontSize = "calc(var(--vh, 1vh) * 1)";
-    div.style.padding = "calc(var(--vh, 1vh) * 0.5)"
-    div.style.backgroundColor = color;
-    div.style.marginRight = "5px";
-    div.textContent = lang;
-    legendTarget.appendChild(div);
-}
+// let legendTarget = document.getElementById("project-legend");
+// for (const [lang, color] of Object.entries(colorField)) {
+//     let div = document.createElement('div');
+//     div.style.width = "calc(var(--vh, 1vh) * 3)";
+//     div.style.height = "calc(var(--vh, 1vh) * 3)";
+//     div.style.textAlign = "center";
+//     div.style.display = "flex";
+//     div.style.justifyContent = "center";
+//     div.style.alignItems = "center";
+//     div.style.fontSize = "calc(var(--vh, 1vh) * 1)";
+//     div.style.padding = "calc(var(--vh, 1vh) * 0.5)"
+//     div.style.backgroundColor = color;
+//     div.style.marginRight = "5px";
+//     div.textContent = lang;
+//     legendTarget.appendChild(div);
+// }
 
 
 
