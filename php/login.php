b4ea7bd5b1d0894323daab0a4a2e212c673b7195
kty990 <masterhotdog9@gmail.com>
2023-03-14 13:13:07 -0600

    Reminder: php won't function until migration

---
+++ php/login.php
@@ -0,0 +1,24 @@
+<?php
+
+// connect to database
+$mysqli = new mysqli("localhost","root","","data");
+
+$query = "SELECT * FROM data WHERE email = '$email' AND pw = '$password'"; 
+// probably should come up with an encryption algorithm for this... (not posted to github)
+
+$result = $mysqli->query($query);
+
+if ($result) {
+    if (mysqli_num_rows($result) > 0) {
+        echo 'found!';
+    } else {
+        echo 'not found';
+    }
+} else {
+    echo 'Error: ' . mysqli_error();
+}
+
+// close connection
+mysqli_close($mysqli);
+
+?>
