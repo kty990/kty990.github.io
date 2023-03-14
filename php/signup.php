0916f887cf842e9b2d70062ffed3e54f9a77ca38
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-03-14 11:10:16 -0600

    Added very basic check for user exists

--- php/signup.php
+++ php/signup.php
@@ -1,4 +1,23 @@
 <?php
 
+// connect to database
+$mysqli = new mysqli("localhost","root","","data");
+
+$query = "SELECT * FROM data WHERE email = '$email'";
+
+$result = $mysqli->query($query);
+
+if ($result) {
+    if (mysqli_num_rows($result) > 0) {
+        echo 'found!';
+    } else {
+        echo 'not found';
+    }
+} else {
+    echo 'Error: ' . mysqli_error();
+}
+
+// close connection
+mysqli_close($mysqli);
 
 ?>
