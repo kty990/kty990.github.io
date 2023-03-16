3762c0842ce8d636fe4b033342cbb77532bc926d
kty990 <masterhotdog9@gmail.com>
2023-03-16 10:33:50 -0600

    Added new font 'Karla'

--- php/request.php
+++ php/request.php
@@ -1,12 +1,12 @@
 <!-- Request a project, used once the user is signed in -->
 <!-- id, status, cost, description -->
+<!-- id, status cannot be modified by the user, only by admin if required through the admin portal which has yet to be developed -->
 
 <?php
 
 $myGetArgs = filter_input_array(INPUT_GET);
-$myPostArgs = filter_input_array(INPUT_POST);
-// $myServerArgs = filter_input_array(INPUT_SERVER);
-// $myCookieArgs = filter_input_array(INPUT_COOKIE);
+$myPostArgs = filter_input_array(INPUT_POST); // using this
+//htmlspecialchars($string, ENT_QUOTES, 'UTF-8');
 
 // connect to database
 $mysqli = new mysqli("localhost","root","","users");
@@ -17,7 +17,7 @@ $result = $mysqli->query($query);
 
 if ($result) {
     if (mysqli_num_rows($result) > 0) {
-        echo $result;
+        echo $result; // JSON encode possibly required here
     } else {
         echo '404 not found';
     }
