ca6cf207663654c26e7d6047e0456fc344b43138
kty990 <masterhotdog9@gmail.com>
2023-03-20 18:04:56 -0600

    .

--- js/site.js
+++ js/site.js
@@ -159,25 +159,32 @@ async function setActive() {
 
 /* Events & Run */
 
-window.addEventListener("resize", (event) => {
-    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
-    if (window.innerWidth <= 1080 && navContainer.id == "nav-container_active") {
-        navContainer.id = "nav-container";
-    }
-});
-
 let vh = window.innerHeight * 0.01;
 let vw = window.innerWidth * 0.01;
+let last_window_resize_check = null;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 document.documentElement.style.setProperty('--vw', `${vw}px`);
 document.documentElement.style.setProperty('--vwh', `${Math.min(vh, vw)}px`);
 
 window.addEventListener('resize', () => {
+    let navContainer = document.getElementById("nav-container") || document.getElementById("nav-container_active");
+    if (window.innerWidth <= 1080 && navContainer.id == "nav-container_active") {
+        navContainer.id = "nav-container";
+    }
+
     let vh = window.innerHeight * 0.01;
     let vw = window.innerWidth * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`);
     document.documentElement.style.setProperty('--vw', `${vw}px`);
     document.documentElement.style.setProperty('--vwh', `${Math.min(vh, vw)}px`);
+
+    let dropdown = document.getElementsByClassName("side-drop")[0] || document.getElementsByClassName("discord-active")[0];
+    if (dropdown.classList.contains("discord-active")) {
+        dropdown.classList.remove("discord-active");
+        dropdown.classList.add("side-drop");
+        discordNav.classList.remove("discord_nav_active");
+        discordNav.classList.add("discord");
+    }
 });
 
 setActive().catch(console.log);
