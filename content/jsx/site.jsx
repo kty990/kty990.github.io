08df4bac8708565380254cfa048a88f8b70c31ed
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-08-31 22:28:12 +0000

    .

--- content/jsx/site.jsx
+++ content/jsx/site.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
 
 import Home from './home.jsx';
