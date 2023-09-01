ecd3f1fd692763ccefb4816b7e753ec2a77fd984
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-09-01 00:42:30 +0000

    .

--- content/jsx/projects.jsx
+++ content/jsx/projects.jsx
@@ -1,25 +1,25 @@
 import pjcts, {Project} from '../js/projects.js';
 import React, { useState, useEffect } from 'react';
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
+// class ErrorBoundary extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { hasError: false };
+//   }
 
-  static getDerivedStateFromError(error) {
-    console.log(error);
-    return { hasError: true };
-  }
+//   static getDerivedStateFromError(error) {
+//     console.log(error);
+//     return { hasError: true };
+//   }
 
-  render() {
-    if (this.state.hasError) {
-      return <div>Something went wrong obtaining the project information.{this.msg}</div>;
-    }
+//   render() {
+//     if (this.state.hasError) {
+//       return <div>Something went wrong obtaining the project information.{this.msg}</div>;
+//     }
 
-    return this.props.children;
-  }
-}
+//     return this.props.children;
+//   }
+// }
 
 function MyProject() {
   const [loaded, setLoaded] = useState(false);
@@ -45,19 +45,16 @@ function MyProject() {
   }, [loaded]);
 
   return (
-    <ErrorBoundary>
-      <div>
+    <div>
         {myProjects.map(project => <div>{project.name}</div>)}
       </div>
-    </ErrorBoundary>
   );
 }
 
 
 function Projects() {
   return (
-    <ErrorBoundary msg="Projects">
-      <div id="project-flex">
+    <div id="project-flex">
         <div id="filter">
           <p id="active">All</p>
           <p>Active</p>
@@ -65,7 +62,6 @@ function Projects() {
         </div>
         <MyProject/>
       </div>
-    </ErrorBoundary>
   );
 }
 
