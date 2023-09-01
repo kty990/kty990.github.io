aa4d74a5092ecb04a94f35529945d7a1d87305c2
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-09-01 19:57:30 +0000

    sdf

--- content/jsx/projects.jsx
+++ content/jsx/projects.jsx
@@ -75,7 +75,7 @@ function MyProject() {
               {Object.entries(project.properties) !== undefined ? (
                 Object.entries(project.properties).map(([key, value]) => {
                   return (
-                    <div style={{ backgroundColor: `${GetColorFromLang(key.toUpperCase())}`, textAlign:"center", width: `calc(${Math.floor(value*10000)/100}% - 0.3vw)`, height: "100%", fontSize: "1vh"}}>
+                    <div style={{ backgroundColor: `${GetColorFromLang(key.toUpperCase())}`, textAlign:"center", width: `calc(${Math.floor(value*10000)/100}% - 0.75%)`, height: "100%", fontSize: "1vh"}}>
                       {key.toUpperCase().replace("JAVASCRIPT","JS")}{'\n'}({Math.floor(value*10000)/100}%)
                     </div>
                   );
