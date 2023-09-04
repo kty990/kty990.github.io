a5ec46c6be174849b80b92f7595d427d9ac4fc7a
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-09-04 15:32:12 +0000

    asdfghjkl

--- content/jsx/projects.jsx
+++ content/jsx/projects.jsx
@@ -1,5 +1,6 @@
 import pjcts, {Project, GetColorFromLang} from '../js/projects.js';
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 class ErrorBoundary extends React.Component {
   constructor(props) {
