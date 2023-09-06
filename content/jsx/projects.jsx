5d3df0103bd7611b834128c32ea514ca7c72945b
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-09-06 01:10:47 +0000

    asdf

--- content/jsx/projects.jsx
+++ content/jsx/projects.jsx
@@ -225,16 +225,19 @@ function Projects() {
               e.style.visibility = "hidden";
             }
           }
+        default:
+          break;
       }
+      
     }
   } 
 
   return (
     <div id="project-flex">
       <div id="filter">
-        <p id="active" className="filter" onClick={applyFilter("All")}>All</p>
-        <p className="filter" onClick={applyFilter("Active")}>Active</p>
-        <p className="filter" onClick={applyFilter("Inactive")}>Inactive</p>
+        <p id="active" className="filter" onclick={applyFilter("All")}>All</p>
+        <p className="filter" onclick={applyFilter("Active")}>Active</p>
+        <p className="filter" onclick={applyFilter("Inactive")}>Inactive</p>
       </div>
       <MyProject/>
     </div>
