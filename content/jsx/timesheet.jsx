0c78041e735d5932cb18e7761c745ad367a21081
phxntxsmic <41134070+kty990@users.noreply.github.com>
2023-09-24 19:12:14 +0000

    jjjhskd

--- content/jsx/timesheet.jsx
+++ content/jsx/timesheet.jsx
@@ -11,7 +11,29 @@ const changeActive = (e) => {
 function Timesheet() {
   document.title = "Timesheet";
   return (
-    <div></div>
+    <div id="timesheet-widget">
+        <div id="calendar">
+            <div id="days">
+                <p>SUN</p>
+                <p>MON</p>
+                <p>TUES</p>
+                <p>WED</p>
+                <p>THURS</p>
+                <p>FRI</p>
+                <p>SAT</p>
+            </div>
+            <div id="display">
+
+            </div>
+        </div>
+        <div id="list">
+            <p id="time">0:00:00</p>
+            {/* List view */}
+        </div>
+        <div>
+            <textarea id="timesheet-input"></textarea>
+        </div>
+    </div>
   );
 }
 
