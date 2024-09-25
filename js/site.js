37d32ae6cfc04704995399c241b05ae815e95a68
kty990 <masterhotdog9@gmail.com>
2024-09-25 12:53:40 -0600

    Added WIP to navbar -> All except home page

--- js/site.js
+++ js/site.js
@@ -5,11 +5,11 @@ async function init() {
     let navbar = `<nav>
     <ul>
         <li><a href="./home" ${(active.indexOf('home') != -1) ? 'id="active"' : ''}>Home</a></li>
-        <li><a href="./about" ${(active.indexOf('about') != -1) ? 'id="active"' : ''}>About</a></li>
-        <li><a href="./flying" ${(active.indexOf('flying') != -1) ? 'id="active"' : ''}>Flying</a></li>
-        <li><a href="./projects" ${(active.indexOf('projects') != -1) ? 'id="active"' : ''}>Projects</a></li>
-        <li><a href="./stories" ${(active.indexOf('stories') != -1) ? 'id="active"' : ''}>Stories</a></li>
-        <li><a href="./timesheet" ${(active.indexOf('timesheet') != -1) ? 'id="active"' : ''}>Timesheet</a></li>
+        <li><a href="./about" ${(active.indexOf('about') != -1) ? 'id="active"' : ''}>About <span style="font-size:0.75vh">WIP</span></a></li>
+        <li><a href="./flying" ${(active.indexOf('flying') != -1) ? 'id="active"' : ''}>Flying <span style="font-size:0.75vh">WIP</span></a></li>
+        <li><a href="./projects" ${(active.indexOf('projects') != -1) ? 'id="active"' : ''}>Projects <span style="font-size:0.75vh">WIP</span></a></li>
+        <li><a href="./stories" ${(active.indexOf('stories') != -1) ? 'id="active"' : ''}>Stories <span style="font-size:0.75vh">WIP</span></a></li>
+        <li><a href="./timesheet" ${(active.indexOf('timesheet') != -1) ? 'id="active"' : ''}>Timesheet <span style="font-size:0.75vh">WIP</span></a></li>
     </ul>
 </nav>`
     document.body.innerHTML += navbar;
